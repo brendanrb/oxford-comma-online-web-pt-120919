b4ebb1fd08e1f46be86c5847f19f35417bf61ed5
Brendan R Borgers <brendanborgers@gmail.com>
2019-12-21 01:18:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
+  big_array = array.join("  , ")
+  another_array = big_array.split
+  almost_done = another_array.insert(-2, " and ")
+  final_array = almost_done.join
+  
+  if array.size == 1
+    return array.join
+    
+    elsif array.size == 2
+    return array.join( " and " )
+
+else 
+  
+  
+return final_array
+end
 
 end
